df894e8a202ae1a67825a2670d3b389482c66901
AheibamBenson <149039742+AheibamBenson@users.noreply.github.com>
2024-07-08 09:42:10 +0000

    Finished the challenge for chapter 4

--- src/Ch04/04_05/CodeDemo.cpp
+++ src/Ch04/04_05/CodeDemo.cpp
@@ -26,10 +26,49 @@ int main(){
 
     // Calculate the GPA for the selected student.
     // Write your code here
+    int i;
+    double total_points;
+    double total_credits;
+    double grade_point = 0;
+    double gpa = 0;
+    for(i = 0; i< grades.size(); i++)
+    {
+        if(id == grades[i].get_student_id())
+        {
+            switch (grades[i].get_grade())
+            {
+            case 'A' : grade_point = 5.0;
+                    break;
+            case 'B' : grade_point = 4.0;
+                    break;
+            case 'C' : grade_point = 3.0;
+                    break;
+            case 'D' : grade_point = 2.0;
+                    break;                        
+            
+            default: grade_point = 0;
+                break;
+            }
+            
 
+            for(int j = 0; j < courses.size(); j++)
+            {
+                if(grades[i].get_course_id() == courses[j].get_id())
+                {
+                    total_points += (grade_point*courses[j].get_credits());
+                    total_credits += courses[j].get_credits();
+                }
+            }
+        }
+    }
+    
+    GPA = total_points/total_credits;
     std::string student_str;
-    student_str = students[0].get_name(); // Change this to the selected student's name
-
+    i = 0;
+    while(i<students.size() && id != students[i].get_id())
+        i++;
+    student_str = students[i].get_name(); // Change this to the selected student's name
+    
     std::cout << "The GPA for " << student_str << " is " << GPA << std::endl;
     
     std::cout << std::endl << std::endl;
